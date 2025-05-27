3d1416e6a3d2bb150778a0f5857c04d59a580187
Yaroslav Kovalevskyi <kovalevskyi96@gmail.com>
2025-05-27 09:32:48 +0300

    made python function applieble for ALL EC2s across the region

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-# ------------------------------------------------------------------------------
-# Lambda Function to STOP/START EC2 Instances with specific TAG
-#
-# Version  Date          Name            Info
-# 1.0      10-Dec-2022   Denis Astahov   Initial Version
-#
-# ------------------------------------------------------------------------------
 import boto3
 import os
 
-EC2TAG_KEY    = os.environ["EC2TAG_KEY"]
-EC2TAG_VALUE  = os.environ["EC2TAG_VALUE"]
-EC2_ACTION    = os.environ["EC2_ACTION"]
+EC2TAG_KEY = os.environ["EC2TAG_KEY"]
+EC2TAG_VALUE = os.environ["EC2TAG_VALUE"]
+EC2_ACTION = os.environ["EC2_ACTION"]
 
 ec2 = boto3.client('ec2')
+
 
 def get_list_of_servers_with_tag(EC2TAG_KEY, EC2TAG_VALUE, EC2_ACTION):
     server_ids = []
@@ -24,26 +18,26 @@ def get_list_of_servers_with_tag(EC2TAG_KEY, EC2TAG_VALUE, EC2_ACTION):
     response = ec2.describe_instances(
         Filters=[
             {
-             'Name'  : "tag:" + EC2TAG_KEY,
-             'Values': [EC2TAG_VALUE]
+                'Name': "tag:" + EC2TAG_KEY,
+                'Values': [EC2TAG_VALUE]
             },
             {
-             'Name'  : "instance-state-name",
-             'Values': instance_state_values
+                'Name': "instance-state-name",
+                'Values': instance_state_values
             }
         ]
     )
-    if len(response['Reservations']) > 0:
-        for server in response['Reservations'][0]['Instances']:
+    for reservation in response['Reservations']:
+        for server in reservation['Instances']:
             server_ids.append(server['InstanceId'])
     return server_ids
 
 
-
-#-----------------START HERE----------------------------------------------------
+# -----------------START HERE---------------
 def lambda_handler(event, context):
     try:
-        server_ids = get_list_of_servers_with_tag(EC2TAG_KEY, EC2TAG_VALUE, EC2_ACTION)
+        server_ids = get_list_of_servers_with_tag(
+            EC2TAG_KEY, EC2TAG_VALUE, EC2_ACTION)
         if len(server_ids) > 0:
             print("Servers to " + EC2_ACTION + ": " + str(server_ids))
 
@@ -58,4 +52,4 @@ def lambda_handler(event, context):
         print("Error occuried! Error Message: " + str(error))
 
     return "DONE"
-#-------------------------------------------------------------------------------
+# ---------------
