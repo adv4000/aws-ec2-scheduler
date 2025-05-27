e8f63fb16f5043881398d80c4fb9c1b1834808c8
Yaroslav Kovalevskyi <kovalevskyi96@gmail.com>
2025-05-27 09:34:33 +0300

    made code more readable (IAM section), increased timeout

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ locals {
     start = var.start_cron_schedule
   }
 }
-#--------------- Lambda---------------------------------------------------------
+#--------------- Lambda ---------------
 resource "aws_lambda_function" "ec2" {
   for_each         = local.scheduler_actions
   function_name    = "${var.name}-to-${each.key}"
@@ -14,8 +14,7 @@ resource "aws_lambda_function" "ec2" {
   handler          = "lambda_function.lambda_handler"
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  tags             = var.tags
-  timeout          = 10
+  timeout          = 30
 
   environment {
     variables = {
@@ -35,58 +34,58 @@ data "archive_file" "lambda_zip" {
   }
 }
 
-#--------------- Lambda IAM Permissions-----------------------------------------
+#--------------- Lambda IAM Permissions ---------------
 resource "aws_iam_role" "lambda" {
-  name               = "${var.name}-iam-role"
-  tags               = var.tags
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": ["lambda.amazonaws.com"]
+  name = "${var.name}-iam-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       },
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
 }
 
+
 resource "aws_iam_policy" "lambda" {
-  name   = "${var.name}-policy"
-  tags   = var.tags
-  policy = <<EOF
-{
-      "Version": "2012-10-17",
-      "Statement": [
-          {
-              "Sid"   : "LoggingPermissions",
-              "Effect": "Allow",
-              "Action": [
-                  "logs:CreateLogGroup",
-                  "logs:CreateLogStream",
-                  "logs:PutLogEvents"
-              ],
-              "Resource": [
-                  "arn:aws:logs:*:*:*"
-              ]
-          },
-          {
-              "Sid"   : "WorkPermissions",
-              "Effect": "Allow",
-              "Action": [
-                  "ec2:DescribeInstances",
-                  "ec2:StopInstances",
-                  "ec2:StartInstances"
-              ],
-              "Resource": "*"
-          }
-      ]
-}
-EOF
+  name = "${var.name}-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "LoggingPermissions"
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = [
+          "arn:aws:logs:*:*:*"
+        ]
+      },
+      {
+        Sid    = "WorkPermissions"
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:StopInstances",
+          "ec2:StartInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_policy_attachment" "lambda" {
@@ -95,21 +94,19 @@ resource "aws_iam_policy_attachment" "lambda" {
   policy_arn = aws_iam_policy.lambda.arn
 }
 
-#---------------Logging---------------------------------------------------------
+#--------------- Logging ---------------
 resource "aws_cloudwatch_log_group" "lambda" {
   for_each          = local.scheduler_actions
   name              = "/aws/lambda/${var.name}-to-${each.key}"
   retention_in_days = 7
-  tags              = var.tags
 }
 
-#--------------- Lambda Triggers------------------------------------------------
+#--------------- Lambda Triggers ---------------
 resource "aws_cloudwatch_event_rule" "ec2" {
   for_each            = local.scheduler_actions
-  name                = "${var.name}-trigger-to-${each.key}-ec2"
+  name                = "${var.name}-trigger-to-${each.key}"
   description         = "Invoke Lambda via AWS EventBridge"
   schedule_expression = each.value
-  tags                = var.tags
 }
 
 resource "aws_lambda_permission" "ec2" {
